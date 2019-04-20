3ae46f38af0013549582d600d24730c5cdc2cbe8
zhangzeqiang <zhangzeqiang@kuaishou.com>
2019-04-20 17:14:01 +0800

    init

---
+++ src/main/java/com/abc/leetcode/two_sum/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Leetcode - two_sum
+ */
+package com.abc.leetcode.two_sum;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
