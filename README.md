b12a672f8eac91e81b72004ba46699fe7a97e0dd
zhangzeqiang <zhangzeqiang@kuaishou.com>
2019-04-20 17:27:20 +0800

    完善项目结构和描述

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # My Leetcode Solutions
 ## Use leetcode-helper tool github:https://github.com/helloShen/leetcode-helper
+### leetcode-helper 使用指南
+#### 修改problem.properties文件中的problem.name属性的值，改为leetcode中问题名。
+#### cmd在跟目录下运行ant generate指令，自动生成上面修改后问题的结构目录，然后在idea中编写即可。
+#### ant compile指令，编译
+#### ant test指令，运行TesterRunner，单元测试
+#### 二合一指令：ant compile test
+#### ant clean指令，清空problem.properties中配置的问题的.java和.class文件
---
+++ src/main/java/com/abc/leetcode/add_two_numbers/Solution2.java
@@ -0,0 +1,21 @@
+/**
+ * Leetcode - add_two_numbers
+ */
+package com.abc.leetcode.add_two_numbers;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
