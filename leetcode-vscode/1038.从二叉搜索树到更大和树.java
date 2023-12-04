c95179d77fc9b347942637694b92cafbdc93218d
zeqiang.zhang <383523842@qq.com>
2023-12-04 09:59:40 +0800

    feat: add solution 1038

---
+++ leetcode-vscode/1038.从二叉搜索树到更大和树.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=1038 lang=java
+ *
+ * [1038] 从二叉搜索树到更大和树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            bstToGst(root.right);
+            sum += root.val;
+            root.val = sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+}
+// @lc code=end
+
